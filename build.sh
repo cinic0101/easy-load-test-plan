d44e0a47a06fb5f32c9d8cd93f7b74ca685031e3
cinic0101 <cinic3615@hotmail.com>
2017-09-25 17:57:18 +0800

    Move sequence

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Mac OS X 64bit
+GOOS=darwin GOARCH=amd64 go build -o out/eztest.mac64 ez-test.go &&
+
 # linux 64bit
 GOOS=linux GOARCH=amd64 go build -o out/eztest.linux64 ez-test.go &&
 
@@ -10,7 +13,4 @@ GOOS=linux GOARCH=386 go build -o out/eztest.linux32 ez-test.go &&
 GOOS=windows GOARCH=amd64 go build -o out/eztest.win64.exe ez-test.go &&
 
 # windows 32bit
-GOOS=windows GOARCH=386 go build -o out/eztest.win32.exe ez-test.go &&
-
-# Mac OS X 64bit
-GOOS=darwin GOARCH=amd64 go build -o out/eztest.mac64 ez-test.go
+GOOS=windows GOARCH=386 go build -o out/eztest.win32.exe ez-test.go

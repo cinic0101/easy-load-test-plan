cbc37da6727e739aa7a99645792c4ffe0773a3e2
Derek <chuanfeng.wu@pousheng.com>
2017-09-25 17:40:57 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# linux 64bit
+GOOS=linux GOARCH=amd64 go build -o out/eztest.linux64 ez-test.go &&
+
+# linux 32bit
+GOOS=linux GOARCH=386 go build -o out/eztest.linux32 ez-test.go &&
+
+# windows 64bit
+GOOS=windows GOARCH=amd64 go build -o out/eztest.win64.exe ez-test.go &&
+
+# windows 32bit
+GOOS=windows GOARCH=386 go build -o out/eztest.win32.exe ez-test.go &&
+
+# Mac OS X 64bit
+GOOS=darwin GOARCH=amd64 go build -o out/eztest.mac64 ez-test.go

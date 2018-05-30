bad63788dc688b40d0dc9270d4113a39bc1511d2
cinic0101 <cinic3615@hotmail.com>
2018-05-30 14:49:27 +0800

    Add version

--- build-gen.sh
+++ build-gen.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+VERSION=0.2
+
 # Mac OS X 64bit
-GOOS=darwin GOARCH=amd64 go build -o out/ezgen.mac64 ez-plan-gen.go &&
+GOOS=darwin GOARCH=amd64 go build -o out/ezgen-mac64-${VERSION} ez-plan-gen.go &&
 
 # linux 64bit
-GOOS=linux GOARCH=amd64 go build -o out/ezgen.linux64 ez-plan-gen.go &&
+GOOS=linux GOARCH=amd64 go build -o out/ezgen-linux64-${VERSION} ez-plan-gen.go &&
 
 # linux 32bit
-GOOS=linux GOARCH=386 go build -o out/ezgen.linux32 ez-plan-gen.go &&
+GOOS=linux GOARCH=386 go build -o out/ezgen-linux32-${VERSION} ez-plan-gen.go &&
 
 # windows 64bit
-GOOS=windows GOARCH=amd64 go build -o out/ezgen.win64.exe ez-plan-gen.go &&
+GOOS=windows GOARCH=amd64 go build -o out/ezgen-win64-${VERSION}.exe ez-plan-gen.go &&
 
 # windows 32bit
-GOOS=windows GOARCH=386 go build -o out/ezgen.win32.exe ez-plan-gen.go
+GOOS=windows GOARCH=386 go build -o out/ezgen-win32-${VERSION}.exe ez-plan-gen.go
--- build-test.sh
+++ build-test.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+VERSION=0.2
+
 # Mac OS X 64bit
-GOOS=darwin GOARCH=amd64 go build -o out/eztest.mac64 ez-test.go &&
+GOOS=darwin GOARCH=amd64 go build -o out/eztest-mac64-${VERSION} ez-test.go &&
 
 # linux 64bit
-GOOS=linux GOARCH=amd64 go build -o out/eztest.linux64 ez-test.go &&
+GOOS=linux GOARCH=amd64 go build -o out/eztest-linux64-${VERSION} ez-test.go &&
 
 # linux 32bit
-GOOS=linux GOARCH=386 go build -o out/eztest.linux32 ez-test.go &&
+GOOS=linux GOARCH=386 go build -o out/eztest-linux32-${VERSION} ez-test.go &&
 
 # windows 64bit
-GOOS=windows GOARCH=amd64 go build -o out/eztest.win64.exe ez-test.go &&
+GOOS=windows GOARCH=amd64 go build -o out/eztest-win64-${VERSION}.exe ez-test.go &&
 
 # windows 32bit
-GOOS=windows GOARCH=386 go build -o out/eztest.win32.exe ez-test.go
+GOOS=windows GOARCH=386 go build -o out/eztest-win32-${VERSION}.exe ez-test.go

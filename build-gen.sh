4879a5eb90f74beb43cc216df1dd3f5aa5baf82e
cinic0101 <cinic3615@hotmail.com>
2018-05-30 14:02:48 +0800

    Seperate 2 build files

---
+++ build-gen.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Mac OS X 64bit
+GOOS=darwin GOARCH=amd64 go build -o out/ezgen.mac64 ez-plan-gen.go &&
+
+# linux 64bit
+GOOS=linux GOARCH=amd64 go build -o out/ezgen.linux64 ez-plan-gen.go &&
+
+# linux 32bit
+GOOS=linux GOARCH=386 go build -o out/ezgen.linux32 ez-plan-gen.go &&
+
+# windows 64bit
+GOOS=windows GOARCH=amd64 go build -o out/ezgen.win64.exe ez-plan-gen.go &&
+
+# windows 32bit
+GOOS=windows GOARCH=386 go build -o out/ezgen.win32.exe ez-plan-gen.go

1b6073e6e5ff6dd604610ad2c2bf435f4d68aaf3
cinic0101 <cinic3615@hotmail.com>
2018-07-12 23:32:54 +0800

    Prototype local storage

--- ui/web.go
+++ ui/web.go
@@ -7,20 +7,31 @@ import (
 
 type Web struct {}
 
-func (w *Web) Start() {
-	router := gin.Default()
+var storage *Storage
 
+func (w *Web) Start() {
+	storage = new(Storage)
+	storage.CreateIfNotExists()
+
+	router := gin.Default()
 	router.LoadHTMLGlob("ui/templates/*")
 	router.Static("/img", "ui/content/image")
 	router.Static("/css", "ui/content/css")
 	router.Static("/js", "ui/content/js")
 	router.GET("/", Index)
+	router.GET("/prototype", Prototype)
 
 	router.Run(":8081")
 }
 
 func Index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H {
-		"title": "Posts",
-	})
+	storage.Load()
+	storage.AddNewTestPlan("1234", "abcd")
+
+	c.HTML(http.StatusOK, "index.html", gin.H {})
 }
+
+func Prototype(c *gin.Context) {
+	c.HTML(http.StatusOK, "prototype.html", gin.H {})
+}
+
